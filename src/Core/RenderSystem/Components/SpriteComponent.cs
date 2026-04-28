c3cb788fc9fc1eeec904fcbbb16672edd760608a
cl1nzet <divashofizov@gmail.com>
2026-04-28 11:43:04 +0500

    feat(Core): add RenderSystem

---
+++ src/Core/RenderSystem/Components/SpriteComponent.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DeltaLitmus.Core.RenderSystem {
+    public struct SpriteComponent {
+        public Texture2D Texture;
+        public Color Color;
+    }
+}
---
+++ src/Core/RenderSystem/Systems/RenderSystem.cs
@@ -0,0 +1,18 @@
+﻿using Arch.Core;
+using DeltaLitmus.Core.TransformSystem;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace DeltaLitmus.Core.RenderSystem {
+    public sealed class RenderSystem {
+        private readonly QueryDescription _renderQuery = new QueryDescription()
+            .WithAll<TransformComponent, SpriteComponent>();
+
+        public void Draw(World world, SpriteBatch sb) {
+            world.Query(in _renderQuery, (ref TransformComponent transform, ref SpriteComponent sprite) => {
+                sb.Begin();
+                sb.Draw(sprite.Texture, transform.Position, sprite.Color);
+                sb.End();
+            });
+        }
+    }
+}
